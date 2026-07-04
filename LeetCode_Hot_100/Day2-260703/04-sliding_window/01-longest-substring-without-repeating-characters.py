f7410ed987a2e423f36e4a7c249a29199590c481
Code_Yang <yiyang.li@mail.sdu.edu.cn>
2026-07-04 16:23:11 +0800

    Day2-260703: 04-sliding_window: 更新最小窗口子串和无重复字符最长子串算法注释 - 添加指针移动的关键思路以增强理解

--- LeetCode_Hot_100/Day2-260703/04-sliding_window/01-longest-substring-without-repeating-characters.py
+++ LeetCode_Hot_100/Day2-260703/04-sliding_window/01-longest-substring-without-repeating-characters.py
@@ -5,6 +5,7 @@
 
 # 核心思路：如果从 A 到 C 都没有重复，那么从 B 到 C 绝对不可能有重复
 # 当我把起点（左手）往右挪一格时，我之前的终点（右手）不需要退回来
+# 两个指针同向移动，维护一个连续区间，每次只变一头，另一头不用回头。
 
 class Solution:
     def length_of_longest_substring(self, string:str) -> int:
