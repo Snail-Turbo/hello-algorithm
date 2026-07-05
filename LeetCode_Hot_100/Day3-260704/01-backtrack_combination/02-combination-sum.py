598e7a00012c5a18eed2a6a02fd8f4838120774c
Code_Yang <yiyang.li@mail.sdu.edu.cn>
2026-07-05 19:40:30 +0800

    Day3-260704: 01-backtrack_combination: 新增组合总和算法 - 实现了基于回溯的组合生成方法，包含详细注释和示例

---
+++ LeetCode_Hot_100/Day3-260704/01-backtrack_combination/02-combination-sum.py
@@ -0,0 +1,69 @@
+"""
+39. 组合总和
+中等
+相关标签
+premium lock icon
+相关企业
+给你一个 无重复元素 的整数数组 candidates 和一个目标整数 target ，找出 candidates 中可以使数字和为目标数 target 的 所有 不同组合 ，并以列表形式返回。你可以按 任意顺序 返回这些组合。
+
+candidates 中的 同一个 数字可以 无限制重复被选取 。如果至少一个数字的被选数量不同，则两种组合是不同的。 
+
+对于给定的输入，保证和为 target 的不同组合数少于 150 个。
+
+ 
+
+示例 1：
+
+输入：candidates = [2,3,6,7], target = 7
+输出：[[2,2,3],[7]]
+解释：
+2 和 3 可以形成一组候选，2 + 2 + 3 = 7 。注意 2 可以使用多次。
+7 也是一个候选， 7 = 7 。
+仅有这两种组合。
+示例 2：
+
+输入: candidates = [2,3,5], target = 8
+输出: [[2,2,2,2],[2,3,3],[3,5]]
+示例 3：
+
+输入: candidates = [2], target = 1
+输出: []
+ 
+
+提示：
+
+1 <= candidates.length <= 30
+2 <= candidates[i] <= 40
+candidates 的所有元素 互不相同
+1 <= target <= 40
+"""
+
+
+class Solution:
+    def combinationSum(self, candidates: list[int], target: int) -> list[list[int]]:
+
+        results, path = [], []  # 这里尽量不要偷懒，连等就是 引用
+
+        def backtrack(current_idx, current_sum):
+            if current_sum > target:  # 有终止条件
+                return
+
+            if current_sum == target:  # 有记录条件
+                results.append(path[:])
+                return
+
+            for i in range(current_idx, len(candidates)):
+                path.append(candidates[i])
+                backtrack(i, current_sum + candidates[i])  # 无限制重复被选取
+
+                path.pop()
+
+        backtrack(0, 0)
+
+        return results
+
+
+candidates = [2, 3, 6, 7]
+target = 7
+so = Solution()
+print(so.combinationSum(candidates, target))
