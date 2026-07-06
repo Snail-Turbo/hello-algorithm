863cf8660116ef9ad138fc304f441afb7edd07f7
Code_Yang <yiyang.li@mail.sdu.edu.cn>
2026-07-06 17:27:21 +0800

    Day4-260706: 02-count_island: 优化岛屿计数算法 - 以广度优先搜索思路，先将输入网格转换为整数矩阵，再逐个遍历网格，使用队列记录访问节点，优化了标记访问的方式，最后返回岛屿数量，实现了岛屿计数的基本功能

--- LeetCode_Hot_100/Day4-260706/02-BFS/02-count_island.py
+++ LeetCode_Hot_100/Day4-260706/02-BFS/02-count_island.py
@@ -8,16 +8,15 @@ class Solution:
         if not grid or len(grid) == 0 or len(grid[0]) == 0:
             return 0
 
-        visited = set()
-
         len_y, len_x = len(grid), len(grid[0])
 
         directions = [[0, -1], [0, 1], [-1, 0], [1, 0]]
-        count = 0
 
         def bfs(y, x):
+
+            # 01. 先初始化队列
             tmp_queue = [(y, x)]
-            grid[y][x] = 0
+            grid[y][x] = 0  # 初始化的入队列就直接处理标记 visited，这里是原地标记
 
             head = 0
             while head < len(tmp_queue):
@@ -26,10 +25,13 @@ class Solution:
 
                 for diff_y, diff_x in directions:
                     new_y, new_x = current_y + diff_y, current_x + diff_x
+                    # 要判断新的 符合界限内；且 未被visited
                     if 0 <= new_y < len_y and 0 <= new_x < len_x and grid[new_y][new_x] == 1:
-                        grid[new_y][new_x] = 0
+
+                        grid[new_y][new_x] = 0  # 【重点】 处理进队列就标记上 visited
                         tmp_queue.append((new_y, new_x))
 
+        count = 0
         for y in range(len_y):
             for x in range(len_x):
                 if grid[y][x] == 1:
