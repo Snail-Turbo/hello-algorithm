0f979059b240a5ea93a40a7d8e535d0c489b3e45
Code_Yang <yiyang.li@mail.sdu.edu.cn>
2026-07-07 17:25:05 +0800

    Day4-260706: 03-DP: 新增硬币兑换问题的动态规划算法实现 - 以动态规划思路，先定义状态数组，再逐步计算状态转移，最后返回结果，实现了硬币兑换问题的基本功能

---
+++ LeetCode_Hot_100/Day4-260706/03-DP/06-coin_change.py
@@ -0,0 +1,24 @@
+class Solution:
+    def coinChange(self, coins: list[int], amount: int) -> int:
+        if amount == 0:
+            return 0
+
+        dp = [amount+1] * (amount+1)  # 必须是 amount+1，因为最多是 amount 个 1
+
+        dp[0] = 0
+
+        for i in range(1, amount+1):
+
+            for coin in coins:
+                if coin > i:  # 只有 i >= coin 才能放   【转移限制】
+                    continue
+                dp[i] = min(dp[i], dp[i-coin]+1)
+
+        return dp[-1] if dp[-1] != amount+1 else -1
+
+
+coins = [1]
+amount = 0
+
+so = Solution()
+print(so.coinChange(coins, amount))
