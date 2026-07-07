e181eccb8ebeb828e6eb024e9c6ad5620f767295
Code_Yang <yiyang.li@mail.sdu.edu.cn>
2026-07-07 13:09:43 +0800

    Day4-260706: 03-DP: 新增打家劫舍算法实现 - 以动态规划思路，先定义状态数组，再逐步计算状态转移，最后返回结果，实现了打家劫舍问题的基本功能

---
+++ LeetCode_Hot_100/Day4-260706/03-DP/03-rob.py
@@ -0,0 +1,31 @@
+class Solution:
+    def rob(self, nums: list[int]) -> int:
+
+        count = 0
+
+        dp = [-1] * (len(nums)+1)
+
+        # f[i] = max(f[i-2] + nums[i], f[i-1])
+        dp[0] = 0
+        dp[1] = nums[0]
+        # f[2] = max(nums[:2])
+
+        def get_nums_index(current_index):
+            return current_index-1
+
+        for end_index in range(2, len(nums) + 1):
+            dp[end_index] = max(dp[end_index-2] + nums[get_nums_index(end_index)], dp[end_index-1])
+
+            # dp[2] = max(dp[0]+nums[1], dp[1])
+            # dp[2] = max(0+2, 1) = 2
+            # dp[3] = max(1+3, 2) = 4
+            # dp[4] = max(2+1, 4) = 4
+
+        return dp[-1]
+
+
+nums1 = [1, 2, 3, 1]
+nums2 = [2, 7, 9, 3, 1]
+so = Solution()
+print(so.rob(nums1))
+print(so.rob(nums2))
