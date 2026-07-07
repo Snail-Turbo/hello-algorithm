b100b35fffea6787c9c1dd9cc702a9e120a986f2
Code_Yang <yiyang.li@mail.sdu.edu.cn>
2026-07-07 15:09:36 +0800

    Day4-260706: 03-DP: 新增最长递增子序列算法实现 - 以动态规划思路，先定义状态数组，再逐步计算状态转移，最后返回结果，实现了最长递增子序列问题的基本功能

---
+++ LeetCode_Hot_100/Day4-260706/03-DP/04-length_of_longest-increase_subarray.py
@@ -0,0 +1,51 @@
+class Solution:
+    def lengthOfLIS(self, nums: list[int]) -> int:
+        # 个人思路：试试从后往前走，以当前nums[index]作为连续数组的开始
+
+        # nums = [0, 1, 0, 3, 2, 3]
+        # len_nums = 6
+        if not nums or len(nums) == 0:
+            return 0
+
+        if len(nums) == 1:
+            return 1
+
+        dp = [0] * (len(nums)+1)  # len_dp = 7
+
+        dp[-1] = 0  # len(nums) = len_dp - 1 = 6
+        dp[-2] = 1  # len(nums)-1 = len_dp -2 = 5
+
+        max_length = 0
+
+        min_impossible = -10001
+        nums.append(min_impossible)
+        # nums = [0, 1, 0, 3, 2, 3, -INF]
+
+        for i in range(len(dp)-3, -1, -1):
+            current_choice = [dp[j]+1 for j in range(i+1, len(dp)-1) if nums[j] > nums[i]]
+            current_choice.append(1)
+            dp[i] = max(current_choice)
+
+            if max_length < dp[i]:
+                max_length = dp[i]
+
+            # dp[4]
+        # dp[i] = max(dp_post + 1 for dp_post in dp[i+1:] if nums[i])
+
+        # dp[i] = max(dp[j]+1 for j in range(i+1, len(nums)) if nums[j]<nums[i])
+        # dp[8] = 0
+        # dp[7] = 1
+        # dp[6] = 1
+        # dp[5] = 2
+        # dp[4] = 3
+
+        return max_length
+
+
+nums = [10, 9, 2, 5, 3, 7, 101, 18]
+nums2 = [0, 1, 0, 3, 2, 3]
+nums3 = []
+so = Solution()
+print(so.lengthOfLIS(nums))
+print(so.lengthOfLIS(nums2))
+print(so.lengthOfLIS(nums3))
