a8cf0759230286b1def7d207b635add11167dd76
Code_Yang <yiyang.li@mail.sdu.edu.cn>
2026-07-08 23:31:41 +0800

    Day5-260708: 02-multi_dp: 新增最长回文子串的动态规划算法实现 - 以动态规划思路，定义状态数组，逐步计算状态转移，最后返回结果，实现了找到字符串中最长回文子串的基本功能

---
+++ LeetCode_Hot_100/Day5-260708/02-multi_dp/03-longest_palindrome.py
@@ -0,0 +1,55 @@
+"""
+5. 最长回文子串
+
+给你一个字符串 s，找到 s 中最长的 回文 子串。
+
+
+示例 1：
+
+输入：s = "babad"
+输出："bab"
+解释："aba" 同样是符合题意的答案。
+示例 2：
+
+输入：s = "cbbd"
+输出："bb"
+
+
+提示：
+
+1 <= s.length <= 1000
+s 仅由数字和英文字母组成
+"""
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        len_s = len(s)
+
+        dp = [[False] * (len_s+1) for _ in range(len_s+1)]
+
+        for i in range(len_s+1):
+            dp[i][i] = True
+
+        max_length = 1
+        max_start, max_end = 1, 1
+        for i in range(len_s-1, 0, -1):
+            for j in range(i+1, len_s+1):
+
+                if j-i == 1:
+                    dp[i][j] = s[i-1] == s[j-1]
+                elif j-i > 1:
+                    dp[i][j] = s[i-1] == s[j-1] and dp[i+1][j-1]
+
+                if dp[i][j] and j-i+1 > max_length:
+                    max_length = j-i+1
+                    max_start, max_end = i, j
+
+        return s[max_start-1:max_end]
+
+
+input_s_1 = "babad"
+input_s_2 = "aaaa"
+so = Solution()
+print(so.longestPalindrome(input_s_1))
+print(so.longestPalindrome(input_s_2))
