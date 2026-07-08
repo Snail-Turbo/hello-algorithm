da1733ab576100cf8683164a30bea189bba8bdec
Code_Yang <yiyang.li@mail.sdu.edu.cn>
2026-07-08 20:34:17 +0800

    Day5-260708: 01-bag: 新增硬币兑换问题的动态规划算法实现 - 以动态规划思路，定义状态数组，逐步计算状态转移，最后返回结果，实现了硬币兑换问题的基本功能

---
+++ LeetCode_Hot_100/Day5-260708/01-bag/02-coin_change.py
@@ -0,0 +1,25 @@
+class Solution:
+    def coinChange(self, coins: list[int], amount: int) -> int:
+
+        len_coins = len(coins)
+
+        dp = [[amount+1] * (amount+1) for _ in range(len_coins+1)]
+
+        for i in range(len_coins+1):
+            dp[i][0] = 0
+
+        for i in range(1, len_coins+1):
+            for j in range(1, amount+1):
+                dp[i][j] = dp[i-1][j]
+
+                if j >= coins[i-1]:
+                    dp[i][j] = min(dp[i][j], dp[i][j-coins[i-1]]+1)
+
+        return dp[len_coins][amount] if dp[len_coins][amount] != amount+1 else -1
+
+
+coins = [1, 2, 5]
+amount = 11
+
+so = Solution()
+print(so.coinChange(coins, amount))
