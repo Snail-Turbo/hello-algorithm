9ff0544fe39e34a1575da48cd1a1bd2cb7996f86
Code_Yang <yiyang.li@mail.sdu.edu.cn>
2026-07-09 14:52:04 +0800

    Day6-260709: 03-tanxin_and_2tree: 新增最大路径和算法实现 - 以深度优先搜索思路，递归遍历二叉树节点，计算每个节点的最大路径和，更新全局最大值，最后返回最大路径和，实现了计算二叉树最大路径和的基本功能

---
+++ LeetCode_Hot_100/Day6-260709/03-tanxin_and_2tree/04-max_path_sum.py
@@ -0,0 +1,40 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxPathSum(self, root: TreeNode) -> int:
+        # 左子树链(或空) -> 拐点 -> 右子树链(或空)
+        if root is None:
+            return 0
+
+        min_impossible = -10**18
+        answer = min_impossible  # 【易错】 answer初始应该初始化为 不可能的小
+
+        def dfs(node) -> int:
+            nonlocal answer
+
+            if node is None:
+                return 0
+
+            current_val = node.val
+
+            # 如果子树 局部最优解小于0，可以不取子树，左右均有可能不取
+            left_max = max(0, dfs(node.left))
+            right_max = max(0, dfs(node.right))
+
+            current_max = left_max + right_max + current_val
+
+            if current_max > answer:
+                answer = current_max
+
+            # 【易错】 返回给父节点的时候，只能传一条子链
+            return current_val + left_max if left_max > right_max else current_val + right_max
+
+        dfs(root)
+
+        return answer
