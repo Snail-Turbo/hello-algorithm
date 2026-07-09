e112433e87501793103d884511f3762bef2dfa82
Code_Yang <yiyang.li@mail.sdu.edu.cn>
2026-07-09 11:12:59 +0800

    Day6-260709: 02-tanxin: 新增分区标签算法实现 - 以贪心和双指针思路，先记录每个字符最晚出现的位置，再遍历字符串更新当前段的最右侧，最后记录每段的长度，实现了将字符串分割为多个标签的基本功能

---
+++ LeetCode_Hot_100/Day6-260709/02-tanxin/04-partition_labels.py
@@ -0,0 +1,25 @@
+class Solution:
+    def partitionLabels(self, s: str) -> list[int]:
+        # 贪心 + 双指针
+
+        last = {}
+        len_s = len(s)
+
+        i = 0
+        for i in range(len_s):  # 先记住每个字符最晚出现是哪个 index
+            last[s[i]] = i
+
+        answer = []
+        left = 0
+        right = 0
+
+        for i in range(len_s):  # 从左向右每个字符遍历
+            current_char = s[i]
+
+            right = max(right, last[current_char])  # 当前这一段最右侧更新
+
+            if i == right:  # 如果当前这一段字符 最晚出现 没有晚于 right
+                answer.append(right-left+1)  # 则当前这一段结束了，记录当前这一段长度 right-left+1
+                left = right + 1  # 下一段的left = right+1
+
+        return answer
