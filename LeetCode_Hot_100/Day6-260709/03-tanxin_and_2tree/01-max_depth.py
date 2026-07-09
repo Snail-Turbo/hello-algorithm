afad38db6a9b7da32c7291b3e8791a94c91b942e
Code_Yang <yiyang.li@mail.sdu.edu.cn>
2026-07-09 11:33:29 +0800

    Day6-260709: 03-tanxin_and_2tree: 新增二叉树最大深度计算实现 - 以深度优先搜索思路，递归遍历二叉树节点，计算每个节点的深度，最后返回最大深度，实现了计算二叉树最大深度的基本功能

---
+++ LeetCode_Hot_100/Day6-260709/03-tanxin_and_2tree/01-max_depth.py
@@ -0,0 +1,82 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+
+        answer = 0
+
+        def dfs(node, depth):
+            nonlocal answer
+
+            if node is None:
+                return
+
+            if node.left is None and node.right is None:
+                if depth > answer:
+                    answer = depth
+                return
+
+            dfs(node.left, depth+1)
+            dfs(node.right, depth+1)
+
+        dfs(root, 1)
+
+        return answer
+
+    def maxDepth2(self, root: TreeNode) -> int:
+        def dfs(node):
+            if node is None:
+                return 0
+
+            return 1 + max(dfs(node.left), dfs(node.right))
+
+        return dfs(root)
+
+    def max_depth(self, root: list) -> int:
+        n = len(root)
+
+        def dfs(index):
+            if index >= n or root[index] is None:
+                return 0
+
+            return 1 + max(dfs(2*index+1), dfs(2*index+2))
+
+        return dfs(0)
+
+
+def seserialize(root_string: str):
+    root_string = root_string.strip()
+
+    if root_string == "[]":
+        return []
+
+    raw_array = root_string[1:-1].split(",")
+
+    root_array = []
+
+    for raw in raw_array:
+        raw = raw.strip()
+
+        if raw in ('null', 'None'):
+            root_array.append(None)
+        else:
+            root_array.append(int(raw))
+
+    return root_array
+
+
+root = " [3,9,20,null,null,15,7]"
+
+root_array = seserialize(root)
+print(root_array)
+
+so = Solution()
+
+print(so.max_depth(root_array))
