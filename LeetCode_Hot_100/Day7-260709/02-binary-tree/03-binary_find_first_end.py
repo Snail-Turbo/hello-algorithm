d4b314228792461ba483f44bb897e17edf49cdcd
Code_Yang <yiyang.li@mail.sdu.edu.cn>
2026-07-12 19:37:59 +0800

    Day7-260709: 02-binary-tree: 实现查找数组中目标值的首次和末次出现 - 通过定义search_first和search_last方法，先初始化左右指针，再通过 while left <= right; 借助 mid_index 在有序数组中查找目标值的首次和末次出现位置，最终返回结果，实现了查找功能。

---
+++ LeetCode_Hot_100/Day7-260709/02-binary-tree/03-binary_find_first_end.py
@@ -0,0 +1,39 @@
+from typing import List
+
+
+class Solution:
+    def search_first(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid_index = (left + right) // 2
+
+            if nums[mid_index] < target:
+                left = mid_index + 1
+            else:
+                right = mid_index - 1
+
+        if left < len(nums) and nums[left] == target:
+            return left
+
+        return -1
+
+    def search_last(self, nums: List[int], target: int) -> int:
+
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid_index = (left + right) // 2
+
+            if target >= nums[mid_index]:
+                left = mid_index + 1
+
+            else:
+                right = mid_index - 1
+
+        if right >= 0 and nums[right] == target:
+            return right
+
+        return -1
