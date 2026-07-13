e194d09189ab77c7d5c9ee6a479b2277b76adcc1
Code_Yang <yiyang.li@mail.sdu.edu.cn>
2026-07-13 17:36:53 +0800

    Day8-260713: 03-is_palindrome: 实现判断链表是否为回文的算法 - 以三种不同思路，分别是使用数组、递归和滑动窗口，先遍历链表获取节点值，再通过比较判断是否为回文，最终返回结果，实现了判断链表回文的功能。

---
+++ LeetCode_Hot_100/Day8-260713-linked_list/03-is_palindrome.py
@@ -0,0 +1,91 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        current = head
+
+        values = []
+
+        while current:
+            values.append(current.val)
+            current = current.next
+
+        left, right = 0, len(values)-1
+
+        while left < right:
+            if values[left] != values[right]:
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
+
+    def isPalindrome_recursive(self, head: ListNode) -> bool:
+        front = head
+
+        def check_back(current: ListNode) -> bool:
+            nonlocal front
+
+            if not current:  # 到最后的时候，current.next = None
+                return True
+
+            if not check_back(current.next):  # 压倒
+                return False
+
+            if front.val != current.val:
+                return False
+
+            front = front.next
+            return True
+
+        return check_back(head)
+
+    def _reverse_linked_list(self, head: ListNode) -> ListNode:
+        already = None
+        current = head
+
+        while current:
+            next = current.next
+            current.next = already  # 如果是奇数个，这里会是中间多的那个，这使得如果123，则2.next = None
+
+            already = current
+
+            current = next
+
+        return already
+
+    def isPalindrome_sliding_window(self, head: ListNode) -> bool:
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        second = self._reverse_linked_list(slow)
+        first = head
+
+        while second:
+            if first.val != second.val:
+                return False
+
+            first = first.next
+            second = second.next
+
+        return True
+
+
+# 1 -> 2 -> 3
+# 会变成：
+# 1 -> 2 <- 3
+
+
+# 1，2，3，4
+# 会变成
+# 1 -> 2 -> 3 <- 4
