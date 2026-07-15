a03ffe7552c65bf151a2ddc6608a3d6f0356f957
Code_Yang <yiyang.li@mail.sdu.edu.cn>
2026-07-15 22:17:25 +0800

    0715机试：02-max_bandwidth；提交版，临时速推版，边缘条件处理wrong

---
+++ daily-practice/26.07/0715/02-max_bandwidth.py
@@ -0,0 +1,80 @@
+
+# If you need to import additional packages or classes, please import here.
+
+def func():
+
+    count, k = map(int, input().strip().split())
+
+    prices = list(map(int, input().strip().split()))
+    type_list = input().strip().split()
+
+    have_map = {}
+    for i in range(count):
+        current_price = prices[i]
+        current_type = type_list[i]
+
+        if current_type in have_map:
+            already_price = have_map[current_type][0]
+            if current_price > already_price:
+                have_map[current_type][0] = current_price
+                have_map[current_type][1] = i
+        else:
+            have_map[current_type] = [prices[i], i]
+
+    # prices = [(prices[index], index) for index in range(len(prices))]
+
+    against_count = int(input())
+    against_map = {}
+    for _ in range(against_count):
+        a, b = input().strip().split()
+        against_map[a] = b
+        against_map[b] = a
+
+    path = set()
+    max_sum = 0
+    results = set()
+
+    def dfs(index, current_sum: int):
+        nonlocal max_sum
+        nonlocal results
+        nonlocal path
+
+        if current_sum > max_sum:
+            max_sum = current_sum  # wei chu li shegnxu bianhao
+            results = set(path)
+
+            return
+
+        if index == len(prices):
+            return
+
+        for type_, node in have_map.items():
+            if against_map.get(type_, "noway") not in path:
+                path.add(type_)
+
+                dfs(index+1, current_sum + node[0])
+
+                path.pop()
+
+    dfs(0, 0)
+
+    print(max_sum)
+    results = [have_map[type__][1] for type__ in list(results)]
+    results.sort()
+
+    print(results[0])
+    for i in range(1, len(results)):
+        print(f" {results[i]}")
+
+
+"""
+6 2
+100 200 150 300 250 180
+n78 n41 n78 n28 n41 n28
+1
+n28 n41
+"""
+
+if __name__ == "__main__":
+
+    func()
