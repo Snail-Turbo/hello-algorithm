acf418ef1329ff00162c9ea28f9f8a6694de03bd
Code_Yang <yiyang.li@mail.sdu.edu.cn>
2026-07-15 22:14:06 +0800

    0715机试：03-源str to target_str；提交版，读题跳读错过 【仅插入】

---
+++ daily-practice/26.07/0715/03-B_to_A.py
@@ -0,0 +1,38 @@
+
+# If you need to import additional packages or classes, please import here.
+
+def func():
+
+    str1 = input()
+    str2 = input()
+
+    len_str_1 = len(str1)
+    len_str_2 = len(str2)
+
+    dp = [[0] * (len_str_2 + 1) for _ in range(len_str_1 + 1)]
+
+    for i in range(len_str_1+1):
+        dp[i][0] = i
+
+    dp[0] = [j for j in range(len_str_2 + 1)]
+
+    for i in range(1, len_str_1 + 1):
+        for j in range(1, len_str_2 + 1):
+
+            if str1[i-1] == str2[j-1]:
+                dp[i][j] = dp[i-1][j-1]
+                continue
+
+            tmp_0 = dp[i][j-1]
+            tmp_1 = dp[i-1][j]
+            tmp_2 = dp[i-1][j-1]
+
+            # choices = (tmp_0, tmp_1, tmp_2)
+
+            dp[i][j] = min(tmp_0, tmp_1, tmp_2) + 1
+
+    print(dp[-1][-1])
+
+
+if __name__ == "__main__":
+    func()
