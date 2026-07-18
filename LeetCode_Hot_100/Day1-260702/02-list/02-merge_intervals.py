5380914ca4510f7af03b1c773f2a525decc04d63
Code_Yang <yiyang.li@mail.sdu.edu.cn>
2026-07-18 13:36:32 +0800

    Day1-260702: 02-list: 在合并区间算法中，增加注释以提高代码可读性，明确合并逻辑和条件判断，实现了更清晰的代码理解。

--- LeetCode_Hot_100/Day1-260702/02-list/02-merge_intervals.py
+++ LeetCode_Hot_100/Day1-260702/02-list/02-merge_intervals.py
@@ -16,9 +16,11 @@ intervals.sort(key=lambda x: x[0])
 merged = []
 
 for interval in intervals:
+    # 若merged为空，或当前区间与merged最后一个区间不重叠，则直接添加
     if not merged or merged[-1][1] < interval[0]:
         merged.append(interval)
     else:
+        # 否则有重叠，合并区间
         merged[-1][1] = max(merged[-1][1], interval[1])
 
 pprint(merged)
