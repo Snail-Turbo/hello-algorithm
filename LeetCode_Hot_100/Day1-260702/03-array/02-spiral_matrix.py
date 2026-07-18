8ca65dff893ebf3827def8ba4eb00d6715727618
Code_Yang <yiyang.li@mail.sdu.edu.cn>
2026-07-18 13:48:16 +0800

    Day1-260702: 03-array: 优化spiral_order函数中的注释和空行，以提高代码可读性，先处理边界条件，再依次遍历矩阵的四个边界，实现了更清晰的逻辑。

--- LeetCode_Hot_100/Day1-260702/03-array/02-spiral_matrix.py
+++ LeetCode_Hot_100/Day1-260702/03-array/02-spiral_matrix.py
@@ -2,16 +2,17 @@
 给你一个 m 行 n 列的矩阵 matrix，请按照 顺时针螺旋顺序 ，返回矩阵中的所有元素
 """
 
+
 def spiral_order(matrix: list[list[int]]) -> list[int]:
     if not matrix:
         return []
-    
+
     m, n = len(matrix), len(matrix[0])
     result = []
 
     top, bottom, left, right = 0, m - 1, 0, n - 1
 
-    while top <= bottom and left <= right: # <= 是为了处理只有一行或一列的情况
+    while top <= bottom and left <= right:  # <= 是为了处理只有一行或一列的情况
         for j in range(left, right + 1):
             result.append(matrix[top][j])
         top += 1
@@ -20,12 +21,12 @@ def spiral_order(matrix: list[list[int]]) -> list[int]:
             result.append(matrix[i][right])
         right -= 1
 
-        if top <= bottom: # 小关键，因为前面的循环可能已经改变了 top 和 bottom 的值
+        if top <= bottom:  # 【小关键】，因为前面的循环可能已经改变了 top 和 bottom 的值
             for j in range(right, left - 1, -1):
                 result.append(matrix[bottom][j])
             bottom -= 1
 
-        if left <= right: # 小关键，因为前面的循环可能已经改变了 left 和 right 的值
+        if left <= right:  # 【小关键】，因为前面的循环可能已经改变了 left 和 right 的值
             for i in range(bottom, top-1, -1):
                 result.append(matrix[i][left])
             left += 1
