889707236b62500e8705739da5b7758338133835
Code_Yang <yiyang.li@mail.sdu.edu.cn>
2026-07-18 13:38:57 +0800

    Day1-260702: 02-list: 优化first_missing_positive和first_missing_positive_inplace函数中的代码格式，调整空行和注释位置，实现了更清晰的代码结构。

--- LeetCode_Hot_100/Day1-260702/02-list/03-first_missing_positive.py
+++ LeetCode_Hot_100/Day1-260702/02-list/03-first_missing_positive.py
@@ -13,31 +13,32 @@
 def first_missing_positive(count, nums):
     if count == 0:
         return 1
-    
+
     st = set(nums)
 
     ans = 1
     while ans in st:
         ans += 1
-    
+
     return ans
+
 
 def first_missing_positive_inplace(count, nums):
     # 关键思路：将数组中的每个元素放到其应该在的位置上，原地交换，最后再遍历一遍数组，找到第一个不在正确位置的元素，其索引+1就是缺失的最小正整数。
     if count == 0:
-        return 1 # 缺少 1 的情况
-    
+        return 1  # 缺少 1 的情况
+
     for i in range(count):
         # 【关键思路2】：
         # while是为了：被交换过来的元素也要检查是否需要回到正确位置；
         # and 后面的条件是为了防止 2个相同的元素在数组中，导致无限来回交换
         while 1 <= nums[i] <= count and nums[nums[i] - 1] != nums[i]:
             nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
-    
+
     for i in range(count):
         if nums[i] != i + 1:
-            return i + 1 # 因为数组索引从0开始，所以要加1
-    
+            return i + 1  # 因为数组索引从0开始，所以要加1
+
     # 缺少 count+1 的情况 【】
     return count + 1
 
